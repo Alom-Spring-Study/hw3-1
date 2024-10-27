e6cc6e6cb3c18ea5d21db343f21f04d2365aca78
Kim na young <kimna041208@gmail.com>
2024-10-27 23:07:43 +0900

    hw3-1

--- src/main/java/com/example/springhw31/repository/UserRepository.java
+++ src/main/java/com/example/springhw31/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.example.springhw31.repository;
 
-public class UserRepository {
+import com.example.springhw31.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 
 }
