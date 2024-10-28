dabdad20956ffa9a182f9c64915c2c012d2adc53
Kim na young <kimna041208@gmail.com>
2024-10-28 23:17:49 +0900

    hw3-1-1

--- src/main/java/com/example/springhw31/entity/User.java
+++ src/main/java/com/example/springhw31/entity/User.java
@@ -14,10 +14,10 @@ public class User {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String username;
 
-  @Column(nullable = false)
+  @Column(nullable = false, unique = true)
   private String password;
 
   @Column(nullable = false)
