021c608133b3fdc0b44ec20048ff9950a12a2934
Uechann <cab07036@naver.com>
2024-11-08 18:24:49 +0900

    hw3-1 완료

--- src/main/java/com/example/springhw31/controller/UserController.java
+++ src/main/java/com/example/springhw31/controller/UserController.java
@@ -1,16 +1,27 @@
 package com.example.springhw31.controller;
 
 import com.example.springhw31.dto.UserDto;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import com.example.springhw31.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 회원가입 메서드처럼 UserDto를 파라미터로 받아주세요
  */
 
+@RestController
+@RequiredArgsConstructor
 public class UserController {
 
+  private final UserService userService;
 
-  public UserDto join(@ModelAttribute UserDto userDto) {
+  @PostMapping("/signup")
+  public UserDto signup(@ModelAttribute UserDto userDto) {
+    return userService.signup(userDto);
+  }
 
+  @GetMapping("/signin")
+  public String signin(@ModelAttribute UserDto userDto) {
+    return userService.signin(userDto);
   }
 }
--- src/main/java/com/example/springhw31/entity/User.java
+++ src/main/java/com/example/springhw31/entity/User.java
@@ -1,14 +1,27 @@
 package com.example.springhw31.entity;
 
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * TODO: 완성된 코드 아닙니다~
  */
+@Entity
+@Getter
+@Setter
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
+  @Column(nullable = false, unique = true)
   private String username;
 
+  @Column(nullable = false)
   private String password;
 
+  @Column(nullable = false, unique = true)
   private String nickname;
 }
