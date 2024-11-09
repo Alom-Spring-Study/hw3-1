a33a2c600fc1bc91b269f133dcd54975824bc2f9
daye200 <daye200@naver.com>
2024-11-09 23:24:18 +0900

    3-1숙제

--- src/main/java/com/example/springhw31/repository/UserRepository.java
+++ src/main/java/com/example/springhw31/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.example.springhw31.repository;
 
-public class UserRepository {
+import com.example.springhw31.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Boolean existsByUsername(String username);
+    Boolean existsByNickname(String nickname);
 }
